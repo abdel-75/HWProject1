a5a5d407b2cddc5890abd41255b7ff49515d8afb
Abdessalam <Abdessalam@192.168.148.117>
2023-04-07 19:23:12 +0000

    initial commit

---
+++ src/hwwtest1/DemoProject1.java
@@ -0,0 +1,12 @@
+package hwwtest1;
+
+public class DemoProject1 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("this is a demo project...");
+		
+		
+		System.out.println(" GIT project");
+	}
+}
